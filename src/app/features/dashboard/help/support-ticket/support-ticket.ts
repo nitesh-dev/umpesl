f959e1b35ce877c3163f7b607110ad3980213ea4
nitesh-dev <niteshdev547@gmail.com>
2025-10-16 15:24:14 +0530

    feat: Implement support ticket feature with stats and table view

--- src/app/features/dashboard/help/support-ticket/support-ticket.ts
+++ src/app/features/dashboard/help/support-ticket/support-ticket.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TableView } from '../../../../shared/components/table-view/table-view';
+import { TopBar } from '../../../../shared/components/top-bar/top-bar';
+import { Button } from '../../../../shared/components/ui/button/button';
+import { CheckBox } from '../../../../shared/components/ui/check-box/check-box';
+import { AddIcon } from '../../../../shared/components/ui/icons/add-icon';
+import { NotificationsIcon } from '../../../../shared/components/ui/icons/notifications-icon';
+import { SearchIcon } from '../../../../shared/components/ui/icons/search-icon';
+import { InputComponent } from '../../../../shared/components/ui/input/input';
 
 @Component({
   selector: 'app-support-ticket',
-  imports: [],
+  imports: [TopBar, Button, AddIcon, NotificationsIcon, CheckBox, InputComponent, SearchIcon, TableView],
   templateUrl: './support-ticket.html',
   styleUrl: './support-ticket.scss'
 })
 export class SupportTicket {
+    stats = signal({
+    pending: 7,
+    delayed: 3,
+    progress: 5,
+    completed: 15
+  })
 
 }
