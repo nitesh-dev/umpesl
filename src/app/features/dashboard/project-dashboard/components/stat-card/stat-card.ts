6590864f4ea3b7b28b4a527c7b3a23ab5fdfeee8
nitesh-dev <niteshdev547@gmail.com>
2025-10-19 09:46:51 +0530

    feat: Refactor stat card component and integrate legends; update project dashboard layout

--- src/app/features/dashboard/project-dashboard/components/stat-card/stat-card.ts
+++ src/app/features/dashboard/project-dashboard/components/stat-card/stat-card.ts
@@ -1,23 +1,17 @@
 import { Component, Input } from '@angular/core';
 
-export interface Legend{
-  pattern: string,
-  label: string
+export interface Legend {
+  pattern: string;
+  label: string;
 }
 
 @Component({
   selector: 'app-stat-card',
   imports: [],
   templateUrl: './stat-card.html',
-  styleUrl: './stat-card.scss'
+  styleUrl: './stat-card.scss',
 })
 export class StatCard {
-
-  @Input() title = "No title"
-  @Input() legends: Legend[] = [{
-    pattern: "blue-pattern",
-    label: 'Hello test'
-  }]
-
-
+  @Input() title = 'No title';
+  @Input() legends: Legend[] = [];
 }
--- src/app/features/dashboard/project-dashboard/project-dashboard.ts
+++ src/app/features/dashboard/project-dashboard/project-dashboard.ts
@@ -5,9 +5,10 @@ import { TopBar } from '@shared/components/top-bar/top-bar';
 import { Button } from '@shared/components/ui/button/button';
 import { AddIcon } from '@shared/components/ui/icons/add-icon';
 import { NotificationsIcon } from '@shared/components/ui/icons/notifications-icon';
-import { ProjectOverallProgressChart } from "@shared/components/charts/project-overall-progress-chart/project-overall-progress-chart";
-import { FinancialTurnoverChart } from "@shared/components/charts/financial-turnover-chart/financial-turnover-chart";
-import { ProcurementStatusChart } from "@shared/components/charts/procurement-status-chart/procurement-status-chart";
+import { ProjectOverallProgressChart } from '@shared/components/charts/project-overall-progress-chart/project-overall-progress-chart';
+import { FinancialTurnoverChart } from '@shared/components/charts/financial-turnover-chart/financial-turnover-chart';
+import { ProcurementStatusChart } from '@shared/components/charts/procurement-status-chart/procurement-status-chart';
+import { Legend, StatCard } from './components/stat-card/stat-card';
 
 @Component({
   selector: 'app-project-dashboard',
@@ -20,8 +21,9 @@ import { ProcurementStatusChart } from "@shared/components/charts/procurement-st
     NotificationsIcon,
     ProjectOverallProgressChart,
     FinancialTurnoverChart,
-    ProcurementStatusChart
-],
+    ProcurementStatusChart,
+    StatCard,
+  ],
   templateUrl: './project-dashboard.html',
   styleUrl: './project-dashboard.scss',
 })
@@ -31,4 +33,38 @@ export class ProjectDashboard {
   pendingDPR = 2;
   originalTO = 87.33;
   currentTO = 90.94;
+
+  installationLegends: Legend[] = [
+    {
+      pattern: 'dark-light-blue-pattern',
+      label: 'Cumulative Planned vs Actual',
+    },
+    {
+      pattern: 'brown-yellow-pattern',
+      label: 'Current Month Planned vs Actual %',
+    },
+    { pattern: 'pink-pattern', label: 'Shortfall' },
+  ];
+
+  financialTurnoverLegends: Legend[] = [
+    {
+      pattern: 'dark-light-blue-pattern',
+      label: 'Planned vs Actual',
+    },
+    {
+      pattern: 'brown-pattern',
+      label: 'Cumulative Turnover Realised',
+    },
+  ];
+
+  procurementStatusLegends: Legend[] = [
+    {
+      pattern: 'dark-light-blue-pattern',
+      label: 'Cumulative Planned vs Actual %',
+    },
+    {
+      pattern: 'brown-yellow-pattern',
+      label: 'Total PO Planned vs Actual%',
+    },
+  ];
 }
